b944a714e2e6124bc2134c47c4bc11fbda109375
mio256 <nao.reiya0215@gmail.com>
2021-10-17 23:54:40 +0900

    スタイル調節

--- board/models.py
+++ board/models.py
@@ -30,8 +30,8 @@ class Thread(models.Model):
         return self.response_set.count()
 
     def print_title(self):
-        if(len(self.thread_text) > 13):
-            return self.thread_text[0:13]+'...'
+        if(len(self.thread_text) > 14):
+            return self.thread_text[0:14]+'...'
         else:
             return self.thread_text
 
