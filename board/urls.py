95124e0508f438df1988acac6df8120818c09f4c
mio256 <nao.reiya0215@gmail.com>
2021-10-18 09:30:29 +0900

    Update urls.py

--- board/urls.py
+++ board/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('list', views.ListView.as_view(), name='list'),
     path('create', views.create_thread, name='create_thread'),
     path('terms', views.TermsView.as_view(), name='terms'),
+    path('explain', views.ExplainView.as_view(), name='explain'),
     path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     path('<int:thread_id>/favorite/', views.add_favorite, name='favorite'),
