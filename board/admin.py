e54ed27e6055960e392ac27c2426099f7fa501a2
mio256 <nao.reiya0215@gmail.com>
2021-10-25 09:07:54 +0900

    Update admin.py

--- board/admin.py
+++ board/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Response, Thread
+from .models import Response, Thread,Question,Answer
 
 
 class ResponseInline(admin.TabularInline):
@@ -14,5 +14,16 @@ class ThreadAdmin(admin.ModelAdmin):
     list_display = ('title', 'update_date', 'responses', 'favorites')
     inlines = [ResponseInline]
 
+class AnswerInline(admin.TabularInline):
+    model = Answer
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_filter = ['update_date']
+    search_fields = ['title']
+    list_display = ('title', 'update_date')
+    inlines = [AnswerInline]
 
 admin.site.register(Thread, ThreadAdmin)
+admin.site.register(Question, QuestionAdmin)
