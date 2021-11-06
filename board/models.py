682516b3fb4242ee09743e55c8a683e3e6375848
mio256 <nao.reiya0215@gmail.com>
2021-11-06 15:59:11 +0900

    ホワイト度 修正

--- board/models.py
+++ board/models.py
@@ -1,10 +1,23 @@
 import datetime
 import re
 import hashlib
+import math
+import csv
+from janome.tokenizer import Tokenizer
 
 from django.db import models
 from django.utils import timezone
 from django.contrib import admin
+
+
+# ネガポジ辞書を読む
+np_dic = {}
+# fp = open("/home/mio256/mysite/pn.csv", "rt", encoding="utf-8")
+fp = open("pn.csv", "rt", encoding="utf-8")
+reader = csv.reader(fp, delimiter='\t')
+for i, row in enumerate(reader):
+    name = row[0]
+    np_dic[name] = row[1]
 
 
 class Thread(models.Model):
@@ -42,6 +55,30 @@ class Thread(models.Model):
     def get_responses(self):
         self.responses = self.response_set.count()
         return self.responses
+
+    def get_pn(self):
+        # 小説を読み込む
+        responses = self.response_set.all()
+        text = ""
+        for i in responses:
+            text += i.content
+
+        # 形態素解析
+        tok = Tokenizer()
+
+        # 数える
+        res = {"p": 0, "n": 0, "e": 0}
+        for t in tok.tokenize(text):
+            bf = t.base_form  # 基本形
+            # 辞書にあるか確認
+            if bf in np_dic:
+                r = np_dic[bf]
+                if r in res:
+                    res[r] += 1
+
+        # 結果を表示
+        cnt = res["p"] + res["n"] + 1
+        return str(math.floor(res["p"] / cnt * 100)) + "%"
 
     def add_favorite(self):
         self.favorites += 1
